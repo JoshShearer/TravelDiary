dbde276358077d179c9700f2a9b5be03b2045afc
Josh Shearer <ShearerJosh@hotmail.com>
2019-12-17 16:31:11 -0800

    Testing a slight modification.

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1>Hi, This is my new Travel Diary App</h1>
+     <h1>Hi, This is the starting point of the travel diary app</h1>
     </div>
   );
 }
