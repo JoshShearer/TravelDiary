e73f1df154839db51d9998054257c808a91f8d1e
Josh Shearer <ShearerJosh@hotmail.com>
2019-12-23 15:48:57 -0800

    Name Change.  Working Structure awaiting more dev.

---
+++ Screens/Entries.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+
+export default class Entries extends Component{
+    render (){
+        return (
+            <h1>This will be the temp screen for bootup and wait...transparent</h1>
+        );
+    }
+}
