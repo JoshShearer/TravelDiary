6f66fa529bfe533159afeaa82d3a031bebab2cb3
Josh Shearer <ShearerJosh@hotmail.com>
2020-01-16 14:01:16 -0800

    Did a rough try at managing state with react components in a central component but found great difficulty.  Will try again using Redux next.

--- Screens/Entries.js
+++ Screens/Entries.js
@@ -1,12 +1,20 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
 import DataContainer from "../Components/EntryContainer";
+import DataHandling from "../data/db/DataHandling";
+import { useStore } from 'react-redux';
 
-export default class Entries extends Component{
-    render (){
-        return (
-            <div>
-                <DataContainer/>
-            </div>
-        );
+export default function Entries (props){  
+
+    const stateChange='false'
+    const data = DataHandling(stateChange);
+    const storeData = useState();
+    return (
+        <div>
+            {data.map(function(singleEntry){
+                return <DataContainer data={singleEntry}/>
+            })}
+            <h1>Component needs to load</h1>
+        </div>
+    );
     }
-}
--- Screens/NewEntry.js
+++ Screens/NewEntry.js
@@ -1,68 +1,203 @@
-import React, { Component } from 'react';
-import styled from "styled-components";
-import ButtonContainer from "../Components/Button";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { ColorButton } from "../Components/Button";
+import ImageHandler from "../Components/ImageHandler";
+import { Link } from "react-router-dom";
+import { connect } from 'react-redux'
+import DataHandling from "../data/db/DataHandling";
 
-export default class NewEntry extends Component{
-        state = {
-            selectedFile: null
-        }
-        fileSelectedHandler = event => {
-            this.setState({
-                selectedFile: event.target.files[0]
-            })
-        }
-        dataEntryHandler = event => {
-            console.log("Adding Entry Data to MongoDB")
-        }
+const NewEntry = () => {
 
-        render (){
+    const stateChange='false'
+    const data = DataHandling(stateChange);  
+  
+    // const [dataState, setDataState] = useState([]);
+    // const [title, setTitle] = useState("");
+    // const [info, setInfo] = useState("");
+    // const [image, setImage] = useState([]);
+    // const [imageType, setImageType] = useState([]);
+    // // const [date, setDate] = useState("");
+    // // const [time, setTime] = useState("");
+    // const [gps, setGPS] = useState([]);
+    // const [intervalIsSet, setIntervalIsSet] = useState(false);
+    // // const [idToDelete, setIdToDelete] = useState(null);
+    // // const [idToUpdate, setIdToUpdate] = useState(null);
+    // // const [updateToApply, setUpdateToApply] = useState(null);
+  
+    // useEffect(() => {
+    //     getDataFromDb();
+    //     if (!intervalIsSet) {
+    //       let interval = setInterval(getDataFromDb, 2000);
+    //       setIntervalIsSet(interval);
+    //     }
+    //     return function unmounting() {
+    //       if (intervalIsSet) {
+    //         clearInterval(intervalIsSet);
+    //         setIntervalIsSet(null);
+    //         console.log("unmounting now");
+    //       }
+    //     };
+    //   }, [intervalIsSet]);                  
+
+    // const getDataFromDb = () => {
+    //   axios
+    //     .get("http://localhost:3001/api/getData")
+    //     .then(res => {
+    //       setDataState([...res.data.data]);
+    //     })
+    //     .catch(err => console.log("axios error", err));
+    // };
+  
+    // const putDataToDB = title => {
+    //   let currentIds = dataState.map(data => data.id);
+    //   let idToBeAdded = 0;
+    //   while (currentIds.includes(idToBeAdded)) {
+    //     ++idToBeAdded;
+    //   }
+    //   const newEntry = {
+    //     id: idToBeAdded,
+    //     title: title,
+    //     info: info,
+    //     img: image,
+    //     imgType: imageType, 
+    //     date: date,
+    //     time: time,
+    //     gps: gps
+    //   };
+    //   setDataState([...dataState, newEntry]);
+    //   console.log(newEntry)
+    //   axios
+    //     .post("http://localhost:3001/api/putData", newEntry)
+    //     .then(res => console.log(res))
+    //     .catch(err => console.log(err));
+    // };
+  
+    // const deleteFromDB = idToDelete => {
+    //   parseInt(idToDelete);
+    //   let objectToDelete = null;
+    //   dataState.forEach(dat => {
+    //     if (dat.id === idToDelete) {
+    //       objectToDelete = dat.id;
+    //     }
+    //   });
+  
+    //   axios.delete("http://localhost:3001/api/deleteData", {
+    //     data: {
+    //       id: objectToDelete
+    //     }
+    //   });
+    // };
+  
+    // const updateDB = (idToUpdate, updateToApply) => {
+    //   let objIdToUpdate = null;
+    //   parseInt(idToUpdate);
+    //   dataState.forEach(dat => {
+    //     if (dat.id === idToUpdate) {
+    //       objIdToUpdate = dat._id;
+    //     }
+    //   });
+  
+    //   axios.post("http://localhost:3001/api/updateData", {
+    //     id: objIdToUpdate,
+    //     update: { title: updateToApply }
+    //   });
+    // };
+    const nLocation = () => {
+        if (navigator.geolocation){
+            navigator.geolocation.getCurrentPosition(getCoordinates);
+        }else{
+            alert("Geolocation is not supported!");
+        }
+    }
+    const getCoordinates = (position) => {
+        setGPS( {lat: position.coords.latitude,
+                long: position.coords.longitude})
+        }    
+    
+    nLocation();
+    var findDate = new Date();
+    var date = (findDate.getMonth()+1) + "-" + findDate.getDate() + "-" + findDate.getFullYear();
+    var time = findDate.getHours() + ":" + findDate.getMinutes() + ":" + findDate.getSeconds();
+    
+    
+    const titleHandler = event => {
+        setTitle(event.target.value)
+    }
+
+    const infoHandler = event => {
+        setInfo(event.target.value)
+    }
+
+    // const imageHandler = async event => {
+    //     const files = event.target.files
+    //     const form = new FormData()
+    //     for (let i=0; i < files.length; i++){
+    //         form.append('files', files[i], files[i].name)
+    //         console.log(form)
+    //     }
+    //     try {
+    //             let request = await fetch('/upload', {
+    //                 method: 'post',
+    //                 body: form,
+    //             })
+    //             const response = await request.json(
+    //             console.log('Response', response)
+    //             )
+    //     }catch (e) {
+    //         alert('Upload Failed')
+    //         console.log('Error upload failed', e)
+    //     }
+    // }   
+
+    
+    
         return (
-            <div className="NewEntry">
+            <form className="NewEntry">
                 <h2>Journal Entry</h2>
+                <input  type="text" 
+                        placeholder="Entry Title" 
+                        value={title}
+                        onChange={titleHandler}
+                        />
+                <br/>
                 Record the moment
                 <br/>
-                <textarea cols="40" name="comments" rows="5"></textarea>
+                <textarea   cols="40" 
+                            name="comments" 
+                            rows="5" 
+                            value={info}
+                            onChange={infoHandler}
+                            >
+                            </textarea>
                 <br/>
                 Date:
-                {cDate}
+                {date}
                 <br/>
                 Time:
-                {cTime}
+                {time}
                 <br/>
-                Location:
-                {cLocation}
+                Location:         
+                <br/>
+                Latitude: 
+                {gps.lat}
+                <br/>
+                Longitude: 
+                {gps.long}
                 <br/>
                 <br/>
-                Add some photos!:
-                <br/>
-                <input type="file" multiple onChange={this.fileSelectedHandler}/>
-                <br/><br/><br/><br/>
-                <ButtonContainer onClick={this.dataEntryHandler}>Entry Finished</ButtonContainer>
-                {/* <button className="button" onClick={this.dataEntryHandler}>Entry Finished</button> */}
-            </div>
+                Add some photos!
+                <ImageHandler className="filepond--item" preview={false}/>
+                <br/><br/>
+                <ColorButton    onClick={() => putDataToDB(title)}
+                                component={Link}
+                                to="/entries"
+                             >
+                    Entry Finished
+                </ColorButton>       
+                
+
+            </form>
         );
-    }
-}
+};
 
-var cLocation = "wt4f";
-function nLocation(){
-    if (navigator.geolocation){
-        function getLocation(position){
-            return ("Lat: " + position.coords.latitude + " Long: " + position.coords.longitude);
-        }
-        navigator.geolocation.getCurrentPosition(getLocation);
-        return getLocation;
-    }
-    else
-    {
-        return ("GPS is not enabled on this device");
-    }
-    return nLocation;
-    // console.log(getLocation);
-}
-
-cLocation = nLocation();
-var findDate = new Date();
-var cDate = (findDate.getMonth()+1) + "-" + findDate.getDate() + "-" + findDate.getFullYear();
-var cTime = findDate.getHours() + ":" + findDate.getMinutes() + ":" + findDate.getSeconds();
-
+export default NewEntry;
