9310f51a6f182bcc60310f00feb989015b093d0d
Josh Shearer <ShearerJosh@hotmail.com>
2020-02-18 22:37:00 -0800

    pending push

--- components/Loading/LoadingSpinner.js
+++ components/Loading/LoadingSpinner.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import classes from './Spinner.css'
+import Spinner from 'react-bootstrap/Spinner';
 
     const LoadingSpinner = () => (
-      <div>
-        <i className="fa fa-spinner fa-spin" /> Loading...
-      </div>
+        <div className={classes.Loader}>Loading...... </div>
+      //   <Spinner
+      //   as="span"
+      //   animation="border"
+      //   size="sm"
+      //   role="status"
+      //   aria-hidden="true"
+      // />
     );
 
     export default LoadingSpinner;
