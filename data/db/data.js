55d901e3bdd7914183be15f3d05334ab90f315bf
Josh Shearer <ShearerJosh@hotmail.com>
2020-02-27 12:11:29 -0800

    Full Data Commited. Ready to post on database.

--- data/db/data.js
+++ data/db/data.js
@@ -8,6 +8,12 @@ export const JournalEntries = [
         date: "07/02/2018",
         time: "12:00",
         gps: {lat: 48.164129, lng: -123.140302},
+        location: {
+            neighborhood: "Dungeness Spit",
+            city: "Sequim",
+            state: "Washington",
+            country: "United States of America"
+        }
     },
     {
         id: 1,
@@ -18,6 +24,12 @@ export const JournalEntries = [
         date: "07/04/2018",
         time: "12:00",
         gps: {lat: 48.696160, lng: -122.619333},
+        location: {
+                    neighborhood: "Summit Trail",
+                    county: "Klamath County",
+                    state: "Washington",
+                    country: "United States of America"
+        }
     },
     {
         id: 2,
@@ -28,6 +40,12 @@ export const JournalEntries = [
         date: "07/08/2018",
         time: "12:00",
         gps: {lat: 45.508819, lng: -122.672262},
+        location: {
+                    neighbourhood: "University District",
+                    city: "Portland",
+                    state: "Oregon",
+                    country: "United States of America"
+        }
     },
     {
         id: 3,
@@ -38,6 +56,12 @@ export const JournalEntries = [
         date: "07/10/2018",
         time: "12:00",
         gps: {lat: 44.060293, lng: -121.307341},
+        location: {
+            neighborhood: "Bend Parkway",
+            city: "Bend",
+            state: "Oregon",
+            country: "United States of America"
+        }
     },
     {
         id: 4,
@@ -48,6 +72,12 @@ export const JournalEntries = [
         date: "07/13/2018",
         time: "12:00",
         gps: {lat: 42.937531, lng: -122.149054},
+        location: {
+            neighborhood: "Summit Trail",
+            county: "Klamath County",
+            state: "Washington",
+            country: "United States of America"
+        }
     },
     {
         id: 5,
@@ -58,6 +88,12 @@ export const JournalEntries = [
         date: "07/16/2018",
         time: "12:00",
         gps: {lat: 41.369269, lng: -124.019056},
+        location: {
+                    neighborhood: "Prairie Creek Trail",
+                    city: "Humboldt County",
+                    state: "California",
+                    country: "United States of America"
+        }
     },
     {
         id: 6,
@@ -68,6 +104,12 @@ export const JournalEntries = [
         date: "07/19/2018",
         time: "12:00",
         gps: {lat: 33.993680, lng: -118.473412},
+        location: {
+            neighborhood: "Venice",
+            city: "Los Angeles",
+            state: "California",
+            country: "United States of America"
+        }
     },
     {
         id: 7,
@@ -78,6 +120,12 @@ export const JournalEntries = [
         date: "07/21/2018",
         time: "12:00",
         gps: {lat: 32.526756, lng: -117.033894},
+        location: {
+            neighbourhood: "Zona Centro",
+            city: "Tijuana",
+            state: "Baja California",
+            country: "Mexico"
+        }
     },
     {
         id: 8,
@@ -88,5 +136,11 @@ export const JournalEntries = [
         date: "07/24/2018",
         time: "12:00",
         gps: {lat: 22.877779, lng: -109.903352},
+        location: {
+            neighborhood: "Grand Solmar Land's End",
+            city: "Cabo San Lucas",
+            state: "Baja California Sur",
+            country: "Mexico"
+        }
     }
 ]
