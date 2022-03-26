309627037beaacfd1a6f307d688c6b07878e36cb
Josh Shearer <ShearerJosh@hotmail.com>
2022-03-26 11:17:11 -0700

    Added from untracked

---
+++ src/components/CustomButtons/EditButton.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {makeStyles} from '@mui/styles';
+import IconButton from '@mui/material/IconButton';
+import EditIcon from '@mui/icons-material/Edit'
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    '& > *': {
+      // margin: theme.spacing(1),
+      float: "right",
+      // backgroundColor: grey[200],
+      // '&:hover': {
+      //   backgroundColor: grey[700],
+  // },
+    },
+  },
+}));
+
+export default function EditButton(props) {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <IconButton aria-label="edit" onClick={props.edit} size="medium">
+        <EditIcon />
+      </IconButton>
+    </div>
+  );
+}
