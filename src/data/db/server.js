37709f2dfa6cf937d0e34f9dec06130745d80ec6
Josh Shearer <ShearerJosh@hotmail.com>
2022-03-29 10:56:49 -0700

    heroku

--- src/data/db/server.js
+++ src/data/db/server.js
@@ -1,7 +1,8 @@
-if (process.env.NODE_ENV !== 'production'){
-    require('dotenv').load()
-}
 const path = require("path");
+if (process.env.NODE_ENV !== 'production'){
+    require('dotenv').config({ path: path.resolve(__dirname, '../../../.env') });
+}
+
 // require('dotenv').config({ path: path.resolve(__dirname, '../../../.env') });
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
