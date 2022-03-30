9439337107efeca7e47122d7f6c83b590f3b2f83
Josh Shearer <ShearerJosh@hotmail.com>
2022-03-29 21:10:45 -0700

    haeroku fails to find /build on the fly.  Pushing to git.

---
+++ client/build/precache-manifest.99eeab6a38565072aeadf4a010b56c0c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac495ae2cd8a5a720651",
+    "url": "/static/css/main.bf9b757c.chunk.css"
+  },
+  {
+    "revision": "ac495ae2cd8a5a720651",
+    "url": "/static/js/main.5811d26e.chunk.js"
+  },
+  {
+    "revision": "37b573d0afefa12cdca6",
+    "url": "/static/js/runtime~main.a00ba4d8.js"
+  },
+  {
+    "revision": "10a10c447421cdf9688c",
+    "url": "/static/css/2.1b5a6218.chunk.css"
+  },
+  {
+    "revision": "10a10c447421cdf9688c",
+    "url": "/static/js/2.6c77a37a.chunk.js"
+  },
+  {
+    "revision": "d79f24b9b8d93f05f714",
+    "url": "/static/js/3.9795b191.chunk.js"
+  },
+  {
+    "revision": "a5e15c83aeb33071e719e3d98cc55872",
+    "url": "/index.html"
+  }
+];
