ff611c7fffca8b81b32de53120c51d46ad2972ac
Josh Shearer <ShearerJosh@hotmail.com>
2022-03-29 20:25:34 -0700

    heroku

--- server.js
+++ server.js
@@ -48,10 +48,14 @@ app.use(logger("dev"));
 
 if(process.env.NODE_ENV === 'production'){
   //set the static folder
-  app.use(express.static('client/build'));
-  // app.get('*', (req, res) => {
-  //   res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
-  // });
+  app.use(express.static(path.join(__dirname, '/client/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  });
+}else{
+  app.get("/", (req, res) => {
+    res.send("api running");
+  })
 }
 
 
